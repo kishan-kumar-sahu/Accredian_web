0bd8076fc16ec5d601a74d5715d4e1dd8abda186
kishan kumar sahu <kumarkishan26636@gmail.com>
2026-04-29 14:07:28 +0530

    update something

--- API/server.js
+++ API/server.js
@@ -1,14 +1,17 @@
 
 import  express from "express"
  import mongoDb from "./DB/config.js"
+   import RequiresRouter  from "./routers/user.js";
  import cors from "cors";
   const app= express();
+
+const port =process.env.PORT || 5000;
 
     import dotenv from "dotenv"
        dotenv.config()
 
-  import RequiresRouter  from "./routers/user.js";
-   const port =process.env.PORT || 3000;
+
+
 
      app.use(express.json())
        app.use(cors())
